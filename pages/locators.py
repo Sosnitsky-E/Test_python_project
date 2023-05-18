bb599b105f8b0f36c4e1dbc1a47d217d5581bc9b
Elena Sosnitsky <sosnitsky.e@gmail.com>
2023-05-18 08:20:47 +0300

    Test add item to the basket added

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,24 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_URL = ("http://selenium1py.pythonanywhere.com/en-gb/accounts/login/")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    LOGIN_FORM_EMAIL_FIELD = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_FORM_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_login-password")
+    LOGIN_FORM_LOGIN_BUTTON = (By.CSS_SELECTOR, 'button[name="login_submit"]')
+    REGISTERED_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTERED_FORM_EMAIL_FIELD = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTERED_FORM_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTERED_FORM_PASSWORD_CONFIRM = (By.CSS_SELECTOR, "#registration-password2")
+    REGISTERED_FORM_REGISTER_BUTTON = (By.CSS_SELECTOR, "button[name='registration_submit']")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    NOTICE_PRODUCT_ADD_SUCCESSFUL = (By.CSS_SELECTOR, '.alertinner ')
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".basket-mini ")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
---
+++ pages/product_page.py
@@ -0,0 +1,27 @@
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_to_basket_button.click()
+
+    def should_add_prodict_successful(self):
+        self.is_element_present(*ProductPageLocators.NOTICE_PRODUCT_ADD_SUCCESSFUL)
+        self.should_notice_content_product_success_message()
+        self.should_total_price_go_up()
+
+    def should_notice_content_product_success_message(self):
+        prod_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        expected_notice_text = f"{prod_name} has been added to your basket."
+        actual_notice_text = self.browser.find_element(*ProductPageLocators.NOTICE_PRODUCT_ADD_SUCCESSFUL).text
+        assert actual_notice_text == expected_notice_text, "Wrong successful notice text"
+
+    def should_total_price_go_up(self, ):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        total_prise_text = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
+        in_st = total_prise_text.find("£")
+        in_end = in_st + 6
+        total_prise = total_prise_text[in_st:in_end]
+        assert total_prise == product_price, "The total price does not go up"
